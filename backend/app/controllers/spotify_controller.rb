b5a8c0caf1a6c6b9b0de30627fb61c45a0c05a21
Grant Ammons <gammons@gmail.com>
2020-09-16 08:06:28 -0400

    implement refresh

--- backend/app/controllers/spotify_controller.rb
+++ backend/app/controllers/spotify_controller.rb
@@ -4,14 +4,30 @@ class SpotifyController < ApplicationController
   # this is called after the spotify redirect.
   # we get a request code, we must exchange for access token
   def authorize
-    return_uri = Rails.env.development? ? "http://localhost:3000" : "https://api.wprb.rocks"
-
     args = {
       grant_type: "authorization_code",
       code: params[:code],
       redirect_uri: (return_uri + "/spotify/authorize")
     }
 
+    call_spotify(args)
+  end
+
+  def refresh
+    return_uri = Rails.env.development? ? "http://localhost:3000" : "https://api.wprb.rocks"
+
+    args = {
+      grant_type: "refresh_token",
+      refresh_token: params[:token],
+      redirect_uri: (return_uri + "/spotify/authorize")
+    }
+
+    call_spotify(args)
+  end
+
+  private
+
+  def call_spotify(args)
     spotify_url = "https://accounts.spotify.com/api/token?" + args.to_query
     spotify_uri = URI.parse(spotify_url)
     auth = Base64.strict_encode64("#{ENV['SPOTIFY_CLIENT_ID']}:#{ENV['SPOTIFY_CLIENT_SECRET']}")
@@ -25,10 +41,10 @@ class SpotifyController < ApplicationController
     resp = http.request(req)
 
     json = JSON.parse(resp.body)
-    return_uri = Rails.env.development? ? "http://localhost:3001" : "https://wprb.rocks"
-
     redirect_to "#{return_uri}?#{json.to_query}"
   end
 
-  def refresh; end
+  def return_uri
+    Rails.env.development? ? "http://localhost:3000" : "https://api.wprb.rocks"
+  end
 end
