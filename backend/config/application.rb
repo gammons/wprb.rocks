8c4ae18e17b71c68f8dfb1ab70e5eefabb25d061
Grant Ammons <grant@grant.dev>
2023-08-18 08:19:30 -0400

    get the damn thing working again

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -1,23 +1,25 @@
-require_relative "boot"
+require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load if %w[development test].include? ENV['RAILS_ENV']
 
 module Backend
   class Application < Rails::Application
@@ -34,6 +36,6 @@ module Backend
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
-    config.time_zone = "Eastern Time (US & Canada)"
+    config.time_zone = 'Eastern Time (US & Canada)'
   end
 end
